779c0d4e47d632e58b02eb6a170f75603f5c17f3
Kevin Cianfarini <kevincianfarini@gmail.com>
2022-07-19 21:55:59 -0400

    Introduce `:event` module (#21)

---
+++ event/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+  alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.kotlin.serialization)
+}
+
+group = "org.climatechangemakers"
+version = "0.0.1"
+
+kotlin {
+
+  linuxX64 {
+    binaries {
+      executable {
+        entryPoint = "org.climatechangemakers.hoa.event.main"
+      }
+    }
+  }
+
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        implementation(libs.kotlinx.serialization.json)
+        implementation(libs.kotlinx.datetime)
+        implementation(project(":multiplatform-aws-lambda-runtime"))
+      }
+    }
+    val commonTest by getting
+  }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,5 @@
 rootProject.name = "hoa-webhook"
 
 include(":attendance")
+include(":event")
 include(":multiplatform-aws-lambda-runtime")
