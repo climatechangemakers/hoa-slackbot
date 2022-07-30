d36113f08ab51c6ffee5bbd4c7f3ee9a757c100b
Kevin Cianfarini <kevincianfarini@gmail.com>
2022-07-30 10:36:33 -0400

    Remove fun keyword

--- multiplatform-aws-lambda-runtime/src/commonMain/kotlin/org/climatechangemakers/lambda/runtime/LambdaHandler.kt
+++ multiplatform-aws-lambda-runtime/src/commonMain/kotlin/org/climatechangemakers/lambda/runtime/LambdaHandler.kt
@@ -5,11 +5,11 @@ import org.climatechangemakers.lambda.model.ApiGatewayResponseV2
 import org.climatechangemakers.lambda.model.RawRequest
 import org.climatechangemakers.lambda.model.RawResponse
 
-public fun interface LambdaHandler<Request : Any, Response : Any> {
+public interface LambdaHandler<Request : Any, Response : Any> {
 
   public suspend fun invoke(request: Request): Response
 }
 
-public fun interface RawLambdaHandler : LambdaHandler<RawRequest, RawResponse>
+public interface RawLambdaHandler : LambdaHandler<RawRequest, RawResponse>
 
-public fun interface ApiGatewayV2LambdaHandler : LambdaHandler<ApiGatewayRequestV2, ApiGatewayResponseV2>
+public interface ApiGatewayV2LambdaHandler : LambdaHandler<ApiGatewayRequestV2, ApiGatewayResponseV2>
